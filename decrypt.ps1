e345d5a800a4cd830980cb3b7416b4c20fff00bd
srothlisberger6361 <39919375+srothlisberger6361@users.noreply.github.com>
2024-03-30 13:19:30 -0500

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -1,4 +1,4 @@
-$byteArray = [IO.File]::ReadAllBytes("$pwd\\test.exe.xored"); 
+$byteArray = [IO.File]::ReadAllBytes("$pwd\\ShellcodeLoader.exe.xored"); 
 $KeyArray = @(90, 15, 69, 23, 21); 
 $keyposition = 0; 
 for ($i = 0; $i -lt $byteArray.count; $i++) 
