033ebed04f4297644d482b1bb3a8f2d803010721
srothlisberger6361 <39919375+srothlisberger6361@users.noreply.github.com>
2024-03-30 12:49:58 -0500

    Create decrypt.ps1

---
+++ decrypt.ps1
@@ -0,0 +1,11 @@
+$byteArray = [IO.File]::ReadAllBytes("$pwd\\test.exe.xored"); 
+$KeyArray = @(90, 15, 69, 23, 21); 
+$keyposition = 0; 
+for ($i = 0; $i -lt $byteArray.count; $i++) 
+{ 
+    $byteArray[$i] = $byteArray[$i] -bxor $KeyArray[$keyposition]; 
+    $keyposition += 1; 
+    if ($keyposition -eq $keyArray.Length) { 
+        $keyposition = 0 ; 
+          } 
+}
